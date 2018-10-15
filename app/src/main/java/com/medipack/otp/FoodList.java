7719aa0984c61500872bbc0e1fd710f08587358a
sneh1212 <kumarsnehshahu@gmail.com>
2018-10-15 17:18:58 +0530

    third

---
+++ app/src/main/java/com/medipack/otp/FoodList.java
@@ -0,0 +1,80 @@
+package com.medipack.otp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.medipack.otp.Interface.ItemClickListener;
+import com.medipack.otp.Model.Food;
+import com.medipack.otp.ViewHolder.FoodViewHolder;
+import com.squareup.picasso.Picasso;
+
+public class FoodList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference foodList;
+
+    String CategoryId="";
+    FirebaseRecyclerAdapter<Food,FoodViewHolder> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+
+        database = FirebaseDatabase.getInstance();
+        foodList = database.getReference("Foods");
+
+        recyclerView = findViewById(R.id.recyler_food);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // get intent here
+        if(getIntent()!=null)
+            CategoryId = getIntent().getStringExtra("CategoryId");
+
+        if(!CategoryId.isEmpty() && CategoryId !=null)
+        {
+            loadListFood(CategoryId);
+        }
+
+    }
+
+    private void loadListFood(String categoryId) {
+                                                                                                                                                    // like: select* from Foods Where MenuId =
+        adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(Food.class,R.layout.food_item,FoodViewHolder.class,foodList.orderByChild("MenuId").equalTo(categoryId)) {
+            @Override
+            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+
+                viewHolder.food_name.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.food_image);
+
+                final Food loacal = model;
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public Void onClick(View view, int position, boolean isLongClick) {
+
+                        Toast.makeText(FoodList.this, ""+loacal.getName(), Toast.LENGTH_SHORT).show();
+                        return null;
+                    }
+                });
+
+            }
+        };
+
+        // set adapter
+        recyclerView.setAdapter(adapter);
+    }
+}
