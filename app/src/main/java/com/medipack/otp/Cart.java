d9a80d27a0fdc98569dd3d350aa4b62333a57a75
sneh1212 <kumarsnehshahu@gmail.com>
2018-10-25 21:10:31 +0530

    third

---
+++ app/src/main/java/com/medipack/otp/Cart.java
@@ -0,0 +1,77 @@
+package com.medipack.otp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.medipack.otp.Database.Database;
+import com.medipack.otp.Model.Order;
+import com.medipack.otp.ViewHolder.CartAdapter;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Cart extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase database;
+    DatabaseReference request;
+
+    TextView txtTotalPrice;
+    Button btnPlace;
+
+    List<Order> cart = new ArrayList<>();
+    CartAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        // Firebase
+        database = FirebaseDatabase.getInstance();
+        request = database.getReference("Requests");
+
+        //init
+
+        recyclerView = (RecyclerView)findViewById(R.id.listCart);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        txtTotalPrice = (TextView)findViewById(R.id.total);
+        btnPlace = (Button)findViewById(R.id.btnPlaceorder);
+
+        loadListFood();
+
+    }
+
+    private void loadListFood() {
+
+        cart = new Database(this).getCarts();
+        adapter = new CartAdapter(cart,this);
+        recyclerView.setAdapter(adapter);
+
+        // calculate total price
+        int total = 0;
+        for (Order order:cart)
+            total+=(Integer.parseInt(order.getPrice()))*(Integer.parseInt(order.getQuantity()));
+
+        Locale locale = new Locale("en","US");
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+
+        txtTotalPrice.setText(fmt.format(total));
+
+    }
+}
